396e9906a5c972e6eef07aac7993ff2ac46c4f17
ryrichard <66878091+ryrichard@users.noreply.github.com>
2022-11-26 22:14:28 -0500

    t

--- Week-10-Streamlit-Web-Apps/env/pages/test.py
+++ Week-10-Streamlit-Web-Apps/env/pages/test.py
@@ -1,18 +1,11 @@
 import pickle
 import streamlit as st
-# Import pandas for data handling
-# import pandas as pd
 
 # NLTK is our Natural-Language-Took-Kit
 import nltk
 from nltk.corpus import stopwords
 from nltk import word_tokenize
-# from nltk.stem import PorterStemmer
-# from nltk.stem import WordNetLemmatizer
-# from nltk.corpus import wordnet
 
-# Libraries for helping us with strings
-# import string
 # Regular Expression Library
 import re
 
